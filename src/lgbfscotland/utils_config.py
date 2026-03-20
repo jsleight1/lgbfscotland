c18c96736bcabc5b1caea175d6def0dc700d660c
Jack Sleight <jacksleight@hotmail.co.uk>
2026-03-20 10:19:03 +0000

    Add settings.toml

--- src/lgbfscotland/utils_config.py
+++ src/lgbfscotland/utils_config.py
@@ -2,7 +2,7 @@ from dynaconf import Dynaconf
 
 settings = Dynaconf(
     envvar_prefix="DYNACONF",
-    settings_files=[".secrets.toml"],
+    settings_files=["settings.toml"],
     environments=True,
     load_dotenv=True,
 )
